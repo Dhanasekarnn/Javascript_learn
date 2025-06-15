2bf7294da95b7582af584e271e686aad13ea3632
Dhanasekar <dstamil19@gmail.com>
2025-06-15 08:40:43 +0530

    string methods

---
+++ String Methods/stringManipulate.js
@@ -0,0 +1,53 @@
+// //charAt
+// let str = "Hello World"
+// console.log(str.charAt(3));
+// console.log(str.charAt(str.length-1));
+
+// // charCodeAt
+// let str1 = "Javascript"
+// console.log(str1.charCodeAt(1)); //give the ascii value 
+
+// //concat
+// let str2 = str.concat(" ",str1)
+// console.log(str2);
+
+//includes
+// let str3 = "Single Threads"
+// console.log(str3.includes("i"));
+// console.log(str3.includes("i", 5)); //index
+
+//indexOf
+
+// let str4 = "Dhanasekar"
+// console.log(str4.indexOf("a"));
+// console.log(str4.indexOf("a", 3));
+// console.log(str4.indexOf("a", 5));
+
+//lastIndexOf
+
+// let str4 = "Dhanasekar"
+// console.log(str4.lastIndexOf("a"));
+// console.log(str4.lastIndexOf("a", 3));
+// console.log(str4.lastIndexOf("a", 5));
+
+//repeat
+// let str5 = "repeatha"
+// console.log(str5.repeat(2));
+
+//replace / replaceAll
+
+// let str6 = "Js is a Script lang - Js"
+// console.log(str6.replace("Js","Javascript"));
+// console.log(str6.replaceAll("Js", "Javascript"));
+
+//Slice
+let str7 = "Single Thread"
+console.log(str7.slice(3));
+console.log(str7.slice(3,9));
+console.log(str7.slice(-3,-1)); // -1 -1 => -2
+
+
+
+
+
+
